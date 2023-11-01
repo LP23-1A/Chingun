883de6a6deccff335c2332941a6bc8843829e6b5
ChingunS <23LP1573@nest.edu.mn>
2023-11-01 12:42:30 +0800

    Added pop up

--- project/index.js
+++ project/index.js
@@ -1,11 +1,36 @@
 
-function countId(id) {
-    var counter = 0;
-    $("card").each(function() {
-      if(this.id==id) {
-         counter+=1;
-       } 
-    });
+const countInToDo = document.querySelectorAll('#toDo #todo').length;
+ console.log(countInToDo); 
+const todo = document.getElementById(countToDo);
+document.getElementById("countToDo").innerHTML = countInToDo;
+
+ const countInProgress = document.querySelectorAll(
+   '#inProgress #todo'
+ ).length;
+ console.log(countInProgress); 
+ document.getElementById("countProgress").innerHTML = countInProgress;
+
+ const countInStuck = document.querySelectorAll(
+   '#stuck #todo'
+ ).length;
+ console.log(countInStuck); 
+ document.getElementById("countStuck").innerHTML = countInStuck;
+
+ const countInDone = document.querySelectorAll(
+   '#done #todo'
+ ).length;
+ console.log(countInDone); 
+ document.getElementById("countDone").innerHTML = countInDone;
+
+ function openForm() {
+   document.getElementById("myForm").style.display = "block";
+   
+ }
+ 
+ function closeForm() {
+   document.getElementById("myForm").style.display = "none";
  }
 
- countId(todo)
+ function addTask() {
+   
+ }
