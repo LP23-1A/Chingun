4ee6e1fb2c4812493fe316278546c5c90715187d
ChingunS <23LP1573@nest.edu.mn>
2023-11-02 12:31:16 +0800

    working on retrieving data from the form

--- project/index.js
+++ project/index.js
@@ -1,36 +1,35 @@
 
 const countInToDo = document.querySelectorAll('#toDo #todo').length;
- console.log(countInToDo); 
-const todo = document.getElementById(countToDo);
 document.getElementById("countToDo").innerHTML = countInToDo;
 
- const countInProgress = document.querySelectorAll(
-   '#inProgress #todo'
- ).length;
- console.log(countInProgress); 
- document.getElementById("countProgress").innerHTML = countInProgress;
+const countInProgress = document.querySelectorAll('#inProgress #todo').length;
+document.getElementById("countProgress").innerHTML = countInProgress;
 
- const countInStuck = document.querySelectorAll(
-   '#stuck #todo'
- ).length;
- console.log(countInStuck); 
- document.getElementById("countStuck").innerHTML = countInStuck;
+const countInStuck = document.querySelectorAll('#stuck #todo').length;
+document.getElementById("countStuck").innerHTML = countInStuck;
 
- const countInDone = document.querySelectorAll(
-   '#done #todo'
- ).length;
- console.log(countInDone); 
- document.getElementById("countDone").innerHTML = countInDone;
+const countInDone = document.querySelectorAll('#done #todo').length; 
+document.getElementById("countDone").innerHTML = countInDone;
 
- function openForm() {
-   document.getElementById("myForm").style.display = "block";
-   
- }
- 
- function closeForm() {
-   document.getElementById("myForm").style.display = "none";
- }
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
- function addTask() {
-   
- }
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+const onSubmit = (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#title").value;
+  const description = document.querySelector("#desc").value;
+  const status = document.querySelector("#status").value;
+  const priority = document.querySelector("#priority").value;
+
+  console.log(title)
+  console.log(description)
+  console.log(status)
+  console.log(priority)
+};
+
