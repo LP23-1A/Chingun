deb623f63356ca766d0f9389178070838abc3d0d
ChingunS <23LP1573@nest.edu.mn>
2023-11-06 12:31:27 +0800

    add task finally works

--- project/index.js
+++ project/index.js
@@ -11,37 +11,66 @@ document.getElementById("countStuck").innerHTML = countInStuck;
 const countInDone = document.querySelectorAll('#done #todo').length; 
 document.getElementById("countDone").innerHTML = countInDone;
 
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-
 function checker() {
   console.log("bob")
   let title = document.getElementById("title").value;
-  let desc = document.getElementById("desc").value;
+  let description = document.getElementById("desc").value;
   let status = document.getElementById("status").value;
   let priority= document.getElementById("priority").value;
   console.log(title)
-  console.log(desc)
+  console.log(description)
   console.log(status)
   console.log(priority)
-  closeForm()
+  addTask(title, description, status, priority)
+  hidePopup()
 }
 
+const popup = document.querySelector('.popup');
+function showPopup() {
+  popup.classList.add('open');
+}
+function hidePopup() {
+  popup.classList.remove('open');
+}
 
-
-document.querySelector(".backdrop").addEventListener("click", () => {
-  document.querySelector(".backdrop").classList.remove("active");
-});
-
-document.querySelectorAll(".add-btn").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector(".backdrop").classList.add("active");
-  });
-});
+function addTask(titleInput, descriptionInput, statusInput, priorityInput) {
+  console.log("check")
+  var card = document.createElement('div');
+  card.setAttribute("id", "card");
+  card.style.marginTop = "8px";
+  card.style.borderRadius = "8px"
+  card.style.padding = "5px"
+  card.style.display = "flex";
+  card.style.gap = "20px"
+  card.className = "card";
+  document.getElementById(statusInput).appendChild(card);
+  var done = document.createElement('div');
+  done.innerHTML = '<i class="fa fa-check circle"></i>';
+  document.getElementById("card").appendChild(done);
+  var details = document.createElement('div');
+  details.setAttribute("id", "details");
+  details.className = "details"
+  var title = document.createElement('header');
+  title.innerHTML = titleInput;
+  var description = document.createElement('p');
+  description.innerHTML = descriptionInput;
+  var priority = document.createElement('span');
+  priority.innerHTML = priorityInput;
+  priority.className = "priority";
+  document.getElementById("card").appendChild(details);
+  document.getElementById("details").appendChild(title);
+  document.getElementById("details").appendChild(description);
+  document.getElementById("details").appendChild(priority);
+  var actions = document.createElement("div");
+  actions.className = "actions";
+  actions.setAttribute("id", "actions");
+  var close = document.createElement("button");
+  close.innerHTML = '<i class="fa fa-close circle"></i>';
+  var edit = document.createElement("button");
+  edit.innerHTML = '<i class="fa fa-edit circle"></i>';
+  document.getElementById("card").appendChild(actions);
+  document.getElementById("actions").appendChild(close);
+  document.getElementById("actions").appendChild(edit);
+}
 
 
