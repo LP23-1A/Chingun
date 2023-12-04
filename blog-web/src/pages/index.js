61e1d9a204a2bc16eeafb4ee2439cfe33bda806b
ChingunS <23LP1573@nest.edu.mn>
2023-12-04 12:40:50 +0800

    ye

--- blog-web/src/pages/index.js
+++ blog-web/src/pages/index.js
@@ -1,10 +1,38 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>balls</div>
+    <main className={`flex justify-between  ,${inter.className}`}>
+      <div >
+        <div>
+          <div>
+            <img src=''></img>
+            <div>
+              <div>
+                <div>
+                  <a>Home</a>
+                </div>
+                <div>
+                  <a>Blog</a>
+                </div>
+                <div>
+                  <a>Contact</a>
+                </div>
+              </div>
+              <div>
+                <div>
+                  search
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </main>
   )
 }
