26deb8b9ae698a7555e1eb29e6b3e7371d11a88f
ChingunS <23LP1573@nest.edu.mn>
2023-12-06 12:28:33 +0800

    idk

--- blog-web/src/pages/components/Header.js
+++ blog-web/src/pages/components/Header.js
@@ -1,31 +1,31 @@
 import React from "react";
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <main>
             <div className='flex px-8 py-8 justify-between items-center flex-1 bg-gradient-to-r from-cyan-500 to-blue-500' >
                 <div className='flex px-0 py-0 justify-between items-center flex-1'>
-                    <div className='flex items-center float-left mr-[118px]' ><img src='/Logo.svg' alt='Logo'></img></div>
+                    <div className='flex items-center float-left mr-[50px]' ><img src='/Logo.svg' alt='Logo'></img></div>
                     <div className='flex justify-center items-center float-right'>
                         <div className='flex justify-center items-center gap-10 w-[50vw]'>
                             <div>
-                                <a>Home</a>
+                                <Link href="../api/Home.js">Home</Link>
                             </div>
                             <div>
-                                <a>Blog</a>
+                                <Link href="../api/AllBlogPosts">Blog</Link>
                             </div>
                             <div>
-                                <a>Contact</a>
+                                <Link href="../api/ContactUs">Contact</Link>
                             </div>
                         </div>
-                        <div className='flex justify-center ml-2'>
+                        <div className='flex justify-center ml-2 w-[118px]'>
                             <div className='flex'>
                                 search
                                 <img src='/search-outline.svg' alt='search icon'></img>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </main>
--- blog-web/src/pages/index.js
+++ blog-web/src/pages/index.js
@@ -1,14 +1,16 @@
 import Image from 'next/image'
-import Header from './components/Header'
+import Header from '../pages/components/Header'
+import Home from './api/Home'
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Index() {
   return (
-    <main className={`w-screen h-auto m-0 px-96 ,${inter.className}`}>
-      <Header />
+    <main>
+      <Home></Home>
+      <Link href={"./api/test"}> pp</Link>
     </main>
   )
 }
