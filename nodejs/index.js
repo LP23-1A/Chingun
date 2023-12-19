967931f16b607b9dcd3a00cc8c5e9fb358e1ceed
ChingunS <chingun713@gmail.com>
2023-12-19 12:07:14 +0800

    nodejs

--- nodejs/index.js
+++ nodejs/index.js
@@ -26,30 +26,45 @@ app.get('/', (req, res) => {
     res.send({success: true, users: users}).end();
 }) 
 
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const filteredData = users.filter((user) => user.id === parseInt(id));
+
+    res.send({success: true, users: filteredData}).end();
+} );
+
 app.post('/', (req, res) => {
     const data = request.body;
     users.push(data);
 
+
     res.send({success: true, users: users}).end();
-    
+
 }) 
 
-app.put('/', (req, res) => {
-    res.send({success: true, method: 'put'}).end();
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+
+    users.map((user) => {
+        if (user.id === parseInt(id)) {
+            console.log(id);
+            user.name = req.body.name;
+        }
+    })
+
+    res.send({success: true, users: users}).end();
+}) 
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    const deletedUserId = users.findIndex((user) => user.id === parseInt(id));
+
+    users = users.slice(0, deletedUserId);
+
+    res.send({success: true, users: users}).end();
 }) 
 
 app.listen(PORT, () => {
     console.log("server running!");
 })
-
-
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
